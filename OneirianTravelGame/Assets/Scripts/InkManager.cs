aecc359fc6d0c43d7524c5c2e8362dfcf80f26fe
DeividVA <david.verdugoabad@gmail.com>
2024-04-09 12:39:08 +0200

    ufs

---
+++ OneirianTravelGame/Assets/Scripts/InkManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
+
+public class InkManager : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private TextAsset _inkJsonAsset;
+    private Story _story;
+
+    [SerializeField]
+    private TextMeshProUGUI _textField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
